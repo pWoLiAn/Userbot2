b6ec912dbe61c33bfa2fb17cc21312c9cd7ec732
pWoLiAn <49567139+pWoLiAn@users.noreply.github.com>
2020-06-23 22:20:31 +0000

    Update scrapers.py

--- userbot/modules/scrapers.py
+++ userbot/modules/scrapers.py
@@ -444,6 +444,41 @@ async def translateme(trans):
             f"Translated some {source_lan.title()} stuff to {transl_lan.title()} just now.",
         )
 
+@register(outgoing=True, pattern="^.tr(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    if "trim" in event.raw_text:
+        # https://t.me/c/1220993104/192075
+        return
+    input_str = event.pattern_match.group(1)
+    if event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        text = previous_message.message
+        lan = input_str or "en"
+    elif "|" in input_str:
+        lan, text = input_str.split("|")
+    else:
+        await event.edit("`.tr LanguageCode` as reply to a message")
+        return
+    text = emoji.demojize(text.strip())
+    lan = lan.strip()
+    translator = Translator()
+    try:
+        translated = translator.translate(text, dest=lan)
+        after_tr_text = translated.text
+        # TODO: emojify the :
+        # either here, or before translation
+        output_str = """**TRANSLATED** from {} to {}
+{}""".format(
+            translated.src,
+            lan,
+            after_tr_text
+        )
+        await event.edit(output_str)
+    except Exception as exc:
+        await event.edit(str(exc))
+
 
 @register(pattern=".lang (trt|tts) (.*)", outgoing=True)
 async def lang(value):
@@ -706,7 +741,7 @@ CMD_HELP.update({
 CMD_HELP.update({
     'trt':
     '.trt <text> [or reply]\
-        \nUsage: Translates text to the language which is set.\nUse .lang trt <language code> to set language for trt. (Default is English)'
+        \nUsage: Translates text to the language which is set.\nUse .lang trt <language code> to set language for trt. (Default is English).also tr.'
 })
 CMD_HELP.update({'yt': '.yt <text>\
         \nUsage: Does a YouTube search.'})
