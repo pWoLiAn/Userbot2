e179dae7cb87fe292738e2784ad211585494a9a0
pWoLiAn <49567139+pWoLiAn@users.noreply.github.com>
2020-07-01 12:26:16 +0000

    Update nsfw.py

--- userbot/modules/nsfw.py
+++ userbot/modules/nsfw.py
@@ -4,6 +4,7 @@
 import os
 import urllib
 import requests
+import nekos
 from asyncio import sleep
 from userbot import bot, CMD_HELP
 from userbot.events import register
@@ -13,12 +14,8 @@ from userbot.events import register
 async def boobs(e):
     await e.edit("`Finding some big boobs...`")
     await sleep(3)
-    await e.edit("`Sending some big boobs...`")
-    nsfw = requests.get('http://api.oboobs.ru/noise/1').json()[0]["preview"]
-    urllib.request.urlretrieve("http://media.oboobs.ru/{}".format(nsfw), "*.jpg")
-    os.rename('*.jpg', 'boobs.jpg')
-    await bot.send_file(e.chat_id, "boobs.jpg")
-    os.remove("boobs.jpg")
+    target = 'pussy'
+    await bot.send_file(e.chat_id, nekos.img(target))
     await e.delete()
 
 @register(outgoing=True, pattern="^\.butts(?: |$)(.*)")
