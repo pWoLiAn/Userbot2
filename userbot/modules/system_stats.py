2cbb7fdd3d084112701e18cf5e59e0bda21db6d7
pWoLiAn <49567139+pWoLiAn@users.noreply.github.com>
2020-07-02 20:35:17 +0000

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -132,9 +132,9 @@ async def pipcheck(pip):
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
     logo = ALIVE_LOGO
-    output=("`Oneechaaan Daaaaaaiskiiiiiiii!!!!`\n\n"
-            f"===================================\n\n"
-            f"`Tis sexy waifu is owned by the noob ni🅱️🅱️a ` {DEFAULTUSER} ` uwu `")
+    output=("`Oneechaaan Daaaaaaiskiiiiiiii!!!!`\n"
+            f"===================================\n"
+            f"`Tis sexy waifu is alive and vibin' : Owned by the noob ni🅱️🅱️a ` {DEFAULTUSER} ` uwu `")
     await bot.send_file(alive.chat_id, logo, caption=output)
     await alive.delete()
 
