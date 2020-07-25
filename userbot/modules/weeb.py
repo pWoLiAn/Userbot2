c4419e13f163a573a6ae1b40d2e8a53ffd830ee6
pWoLiAn <49567139+pWoLiAn@users.noreply.github.com>
2020-07-25 18:01:19 +0000

    Update weeb.py

--- userbot/modules/weeb.py
+++ userbot/modules/weeb.py
@@ -88,6 +88,7 @@ async def ssearch(event):
        description = json.get('description', 'N/A').replace('<i>', '').replace('</i>', '').replace('<br>', '')
        msg += shorten(description, info) 
        image = json.get('bannerImage', None)
+       image = image.replace('(','').replace(')','')
        if image:
                msg += f" [〽️]({image})"
                await event.edit(msg, link_preview = True)
