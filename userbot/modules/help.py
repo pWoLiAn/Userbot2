6254a1bf4bd41144154094f6f81fb3d5eb45ed89
pWoLiAn <49567139+pWoLiAn@users.noreply.github.com>
2020-07-29 19:17:13 +0530

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -13,7 +13,6 @@ from userbot import bot
 @register(outgoing=True, pattern="^.help(?: |$)(.*)")
 async def help(event):
     """ For .help command,"""
-    tgbotusername = TG_BOT_USER_NAME_BF_HER
     args = event.pattern_match.group(1).lower()
     if args:
         if args in CMD_HELP:
