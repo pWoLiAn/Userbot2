58537ece11365edb250d91a005af15fa73bbc834
pWoLiAn <49567139+pWoLiAn@users.noreply.github.com>
2020-07-29 18:16:04 +0530

    Update heroku.py

--- userbot/modules/heroku.py
+++ userbot/modules/heroku.py
@@ -187,10 +187,34 @@ async def dyno_usage(dyno):
                  f"-  {percentage}%**"
             )
             return True
+         
+@register(outgoing=True, pattern=r"^\.logs")
+async def _(dyno):
+    try:
+        Heroku = heroku3.from_key(HEROKU_API_KEY)
+        app = Heroku.app(HEROKU_APP_NAME)
+    except BaseException:
+        return await dyno.reply(
+            "`Please make sure your Heroku API Key, Your App name are configured correctly in the heroku var.`"
+        )
+    await dyno.edit("`Getting Logs....`")
+    with open("logs.txt", "w") as log:
+        log.write(app.get_log())
+    fd = codecs.open("logs.txt", "r", encoding="utf-8")
+    data = fd.read()
+    key = (requests.post("https://nekobin.com/api/documents",
+                         json={"content": data}) .json() .get("result") .get("key"))
+    url = f"https://nekobin.com/raw/{key}"
+    await dyno.edit(f"`Here the heroku logs:`\n\nPasted to: [Nekobin]({url})")
+    return os.remove("logs.txt")
+
+
 CMD_HELP.update({
     "heroku":
 "`.usage`"
 "\nUsage: Check your heroku dyno hours remaining"
+"\n\n`.logs`"
+"\nUsage: Gets heroku logs in nekobin"  
 "\n\n`.set var` <NEW VAR> <VALUE>"
 "\nUsage: add new variable or update existing value variable"
 "\n!!! WARNING !!!, after setting a variable the bot will restarted"
