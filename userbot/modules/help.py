92c7de8ef4802aeb9288687bac78ecdea898af73
pWoLiAn <49567139+pWoLiAn@users.noreply.github.com>
2020-07-30 10:18:32 +0530

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -25,5 +25,5 @@ async def help(event):
         string = ""
         for i in sorted(CMD_HELP):
          string += "`" + str(i)
-         string += "`  ┻━┻  "
+         string += "`  <>  "
         await event.reply(string)
