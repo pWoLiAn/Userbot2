59749c2ed6fb03231edef1df8584b12452a4894e
pWoLiAn <49567139+pWoLiAn@users.noreply.github.com>
2020-07-30 20:00:55 +0000

    Update weeb.py

--- userbot/modules/weeb.py
+++ userbot/modules/weeb.py
@@ -8,8 +8,6 @@ import math
 import time
 import random
 import requests
-from jikanpy import Jikan
-from jikanpy.exceptions import APIException
 from urllib.parse import quote as urlencode
 from telethon import events
 from userbot.events import register
@@ -18,7 +16,6 @@ from asyncio import sleep
 
 
 _pats = []
-jikan = Jikan()
 
 def shorten(description, info = 'anilist.co'):
     msg = "" 
@@ -106,7 +103,7 @@ anime_query = '''
 url = 'https://graphql.anilist.co'
 
 
-@register(outgoing=True, pattern=r"^.airing(.*)")
+@register(outgoing=True, pattern=r"^.air(.*)")
 async def airing(event):
   message = event.pattern_match.group(1)
   message = ' ' + message
@@ -126,7 +123,7 @@ async def airing(event):
   await event.edit(msg)
 
 
-@register(outgoing=True, pattern=r"^.anime(.*)")
+@register(outgoing=True, pattern=r"^.ani(.*)")
 async def ssearch(event):
    message = event.pattern_match.group(1)
    message = ' ' + message
@@ -154,65 +151,6 @@ async def ssearch(event):
           await event.edit(msg)
 
 
-@register(outgoing=True, pattern=r"^.sanime (.*)")
-async def asearch(event):
-    msg = event.pattern_match.group(1)
-    res = ""
-    try:
-        res = jikan.search("anime", msg)
-    except APIException:
-        await event.edit("Error connecting to the API. Please try again!")
-        return ""
-    try:
-        res = res.get("results")[0].get("mal_id") # Grab first result
-    except APIException:
-        await event.edit("Error connecting to the API. Please try again!")
-        return ""
-    if res:
-        anime = jikan.anime(res)
-        title = anime.get("title")
-        japanese = anime.get("title_japanese")
-        type = anime.get("type")
-        duration = anime.get("duration")
-        synopsis = anime.get("synopsis")
-        source = anime.get("source")
-        status = anime.get("status")
-        episodes = anime.get("episodes")
-        score = anime.get("score")
-        rating = anime.get("rating")
-        genre_lst = anime.get("genres")
-        genres = ""
-        for genre in genre_lst:
-            genres += genre.get("name") + ", "
-        genres = genres[:-2]
-        studios = ""
-        studio_lst = anime.get("studios")
-        for studio in studio_lst:
-            studios += studio.get("name") + ", "
-        studios = studios[:-2]
-        duration = anime.get("duration")
-        premiered = anime.get("premiered")
-        image_url = anime.get("image_url")
-        url = anime.get("url")
-        trailer = anime.get("trailer_url")
-    else:
-        await event.edit("No results found!")
-        return
-    rep = f"<b>{title} ({japanese})</b>\n"
-    rep += f"<b>Type:</b> <code>{type}</code>\n"
-    rep += f"<b>Source:</b> <code>{source}</code>\n"
-    rep += f"<b>Status:</b> <code>{status}</code>\n"
-    rep += f"<b>Genres:</b> <code>{genres}</code>\n"
-    rep += f"<b>Episodes:</b> <code>{episodes}</code>\n"
-    rep += f"<b>Duration:</b> <code>{duration}</code>\n"
-    rep += f"<b>Score:</b> <code>{score}</code>\n"
-    rep += f"<b>Studio(s):</b> <code>{studios}</code>\n"
-    rep += f"<b>Premiered:</b> <code>{premiered}</code>\n"
-    rep += f"<b>Rating:</b> <code>{rating}</code>\n\n"
-    rep += f"<a href='{image_url}'>\u200c</a>"
-    rep += f"<i>{synopsis}</i>\n"
-    await event.edit(rep, parse_mode='html', link_preview = True)
-    
 
 @register(outgoing=True, pattern=r"^.pat(?: |$)")
 async def pat(e):
@@ -275,8 +213,8 @@ CMD_HELP.update({
     "\nUsage: Get random pat gif.\n"
     "`.cum`"
     "\nUsage: Get random cum gif.\n"
-    "`.anime`"
+    "`.ani`"
     "\nUsage: Get details about given anime.\n"
-    "`.airing`"
+    "`.air`"
     "\nUsage: Get airing details about given currently airing anime."
 })
