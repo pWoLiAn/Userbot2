a41d195f612f414ba3d7a76556681ac522128853
pWoLiAn <49567139+pWoLiAn@users.noreply.github.com>
2020-08-21 18:08:20 +0000

    Update memes.py

--- userbot/modules/memes.py
+++ userbot/modules/memes.py
@@ -22,7 +22,7 @@ from telethon.tl.types import MessageEntityMentionName
 
 from cowpy import cow
 
-from userbot import CMD_HELP, LOGS, BOTLOG, bot
+from userbot import CMD_HELP, LOGS, BOTLOG, bot, BOTLOG_CHATID
 from userbot.events import register
 
 # ================= CONSTANT =================
